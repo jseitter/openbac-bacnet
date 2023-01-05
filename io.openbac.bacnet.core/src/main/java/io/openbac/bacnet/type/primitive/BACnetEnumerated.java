f1cf58e4b606d74b7ef782c437ea2daa45993231
J. Seitter <oss@seitter.net>
2023-01-05 21:13:37 +0100

    made enumareated a own class not extending unsigned integer

--- io.openbac.bacnet.core/src/main/java/io/openbac/bacnet/type/primitive/BACnetEnumerated.java
+++ io.openbac.bacnet.core/src/main/java/io/openbac/bacnet/type/primitive/BACnetEnumerated.java
@@ -1,13 +1,51 @@
 package io.openbac.bacnet.type.primitive;
 
-public class BACnetEnumerated extends BACnetUnsignedInteger {
+import io.netty.buffer.ByteBuf;
+
+public class BACnetEnumerated extends BACnetPrimitive {
+
+	private long valueInteger = 0;
 
 	public BACnetEnumerated(int valueInteger) {
-		super(valueInteger);
+		if (valueInteger < 0) {
+			throw new IllegalArgumentException("Value has to be unsigned!");
+		}
+		this.valueInteger = valueInteger;
 	}
-	
-	public BACnetEnumerated(long valueInteger) {
-		super(valueInteger);
+
+	public BACnetEnumerated(final ByteBuf data) {
+		buf = data;
+		decodeTag();
+
+		if (tagLVT > 0) {
+			if (tagLVT == 1) {
+				this.valueInteger = buf.readUnsignedByte();
+			} else if (tagLVT == 2) {
+				this.valueInteger = buf.readUnsignedShort();
+			} else if (tagLVT == 4) {
+				this.valueInteger = buf.readUnsignedInt();
+			}
+
+		} else {
+			throw new IllegalArgumentException("Invalid unsigned integer data");
+		}
+
+	}
+
+	public int getValue() {
+		return (int) valueInteger;
+	}
+
+	@Override
+	public void encodeApplication(ByteBuf data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void encode(ByteBuf buf, int contextId) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
