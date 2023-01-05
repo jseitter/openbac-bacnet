d7a30e8a703f804b63c479933afa3a87b8fe6d6b
J. Seitter <oss@seitter.net>
2023-01-05 21:44:07 +0100

    reworked unconfirmed services and intial Iam and whois

--- io.openbac.bacnet.core/src/main/java/io/openbac/service/unconfirmed/TimeSynchronizationService.java
+++ io.openbac.bacnet.core/src/main/java/io/openbac/service/unconfirmed/TimeSynchronizationService.java
@@ -1,11 +1,33 @@
-//package io.openbac.service.unconfirmed;
-//
-//import io.openbac.service.IServiceChoiceType;
-//import io.openbac.service.ServiceChoiceTypes.UnconfirmedServiceChoiceType;
-//
-//public class TimeSynchronizationService extends BACnetUnconfirmedService {
-//
-//	@Override
-//	public IServiceChoiceType getServiceChoiceType() {
-//		return UnconfirmedServiceChoiceType.TIME_SYNCHRONIZATION;
-//	}
+package io.openbac.service.unconfirmed;
+
+import io.netty.buffer.ByteBuf;
+import io.openbac.bacnet.exceptions.BACnetParseException;
+import io.openbac.bacnet.type.primitive.BACnetDate;
+import io.openbac.bacnet.type.primitive.BACnetTime;
+
+/**
+ * Service to send data and time information to other BACnet Systems
+ * 
+ * @author Joerg Seitter
+ *
+ */
+public class TimeSynchronizationService {
+	
+	BACnetDate date;
+	BACnetTime time;
+	
+	public TimeSynchronizationService(BACnetDate date, BACnetTime time) {
+		this.date=date;
+		this.time=time;
+	}
+		
+	
+	public TimeSynchronizationService(final ByteBuf buf) throws BACnetParseException {
+		date= BACnetDate.createPrimitive(BACnetDate.class, buf);
+	}
+	
+	
+
+}
+
+
