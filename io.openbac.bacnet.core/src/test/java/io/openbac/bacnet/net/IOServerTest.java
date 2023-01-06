c3d7f1febcc6f44e1a7ccbd73b079cf0a8d73463
J. Seitter <oss@seitter.net>
2023-01-06 13:40:33 +0100

    initial IAm Serive Test and a testcase for running the IO Server

---
+++ io.openbac.bacnet.core/src/test/java/io/openbac/bacnet/net/IOServerTest.java
@@ -0,0 +1,31 @@
+package io.openbac.bacnet.net;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class IOServerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() throws IOException, InterruptedException {
+		
+		BACnetIOServer srv = new BACnetIOServer("192.168.60.26", BACnetIOServer.BACNET_DEFAULT_PORT0);
+		srv.run();
+		
+	}
+
+}
+ 
---
+++ io.openbac.bacnet.core/src/test/java/io/openbac/bacnet/service/BACnetIAmServiceTest.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package io.openbac.bacnet.service;
+
+import java.io.InputStream;
+import java.util.HashMap;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.netty.buffer.ByteBuf;
+import io.openbac.bacnet.exceptions.BACnetParseException;
+import io.openbac.bacnet.testutil.BACnetTest;
+import io.openbac.bacnet.testutil.TestDataLoader;
+import io.openbac.service.unconfirmed.BACnetIAmService;
+import io.openbac.service.unconfirmed.BACnetUnconfirmedService;
+
+/**
+ * @author J. Seitter
+ *
+ */
+public class BACnetIAmServiceTest extends BACnetTest {
+
+	
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		// get the testdata
+		InputStream testdata = BACnetIAmServiceTest.class.getClassLoader()
+				.getResourceAsStream("io/openbac/bacnet/service/BACnetIAmService.td");
+		loader = new TestDataLoader(testdata);
+	}
+
+	@Test
+	public void testDecode() throws BACnetParseException {
+
+		int size = loader.resultBuffers.size();
+		System.out.println("will run "+size+" tests");
+		for (int i = 0; i < size; i++) {
+			System.out.println("------------ running case # "+i+" -------------");
+			ByteBuf buf = loader.resultBuffers.get(i);
+			HashMap<String, String> props = loader.resultProps.get(i);
+
+			buf.readByte(); // skip PDU Type
+			buf.readByte(); // skip service choice
+ 			BACnetIAmService srv = BACnetUnconfirmedService.create(BACnetIAmService.class, buf);
+			
+			System.out.println(srv.toString());
+			
+		}
+	}
+	
+
+	@Test
+	public void testEncode() {
+	
+	}
+}
