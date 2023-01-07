b7fc2968d7d0b11d0039e780831113a9b93db4b9
J. Seitter <oss@seitter.net>
2023-01-07 14:25:41 +0100

    initial implementation of encoding

--- io.openbac.bacnet.core/src/main/java/io/openbac/service/confirmed/BACnetReadPropertyService.java
+++ io.openbac.bacnet.core/src/main/java/io/openbac/service/confirmed/BACnetReadPropertyService.java
@@ -1,34 +1,148 @@
 package io.openbac.service.confirmed;
 
 import io.netty.buffer.ByteBuf;
+import io.openbac.bacnet.type.BACnetAny;
 import io.openbac.bacnet.type.enumerated.BACnetPropertyIdentifier;
 import io.openbac.bacnet.type.primitive.BACnetObjectIdentifier;
 import io.openbac.bacnet.type.primitive.BACnetUnsignedInteger;
 
-public class ReadPropertyService {
+/**
+ * Handles the ReadProperty Service
+ * @author joerg
+ *
+ */
+public class BACnetReadPropertyService extends BACnetConfirmedService{
 
-	// Structure of the ReadProperty Service
-	// [0] BACnetObjectIdentifier
-	private  BACnetObjectIdentifier objectIdentifier;
-	// [1] BACnetPropertyIdentifier
-	private BACnetPropertyIdentifier propertyIdentifier;
-	// [2] Unsigned OPTIONAL
-	private BACnetUnsignedInteger propertyArrayIndex;
-	
-	
 	/**
-	 * writes the encoded service into the ByteBuffer
-	 * starting with the Service Choice Element of the APDU
-	 * @param data
+	 * Request for ReadProperty
+	 * @author Joerg Seitter
+	 *
 	 */
-	public void encode(final ByteBuf data) {
-		// write service choice
-		data.writeByte(ConfirmedServiceChoice.READ_PROPERTY.value);
-		objectIdentifier.encode(data,0);
-		propertyIdentifier.encode(data,1);
+	public static class ReadPropertyRequest {
+		// Structure of the ReadProperty Request
+		// [0] BACnetObjectIdentifier
+		private  BACnetObjectIdentifier objectIdentifier;
+		// [1] BACnetPropertyIdentifier
+		private BACnetPropertyIdentifier propertyIdentifier;
+		// [2] Unsigned OPTIONAL
+		private BACnetUnsignedInteger propertyArrayIndex;
+		
+		
+		
+		
+		public BACnetObjectIdentifier getObjectIdentifier() {
+			return objectIdentifier;
+		}
+
+
+
+		public void setObjectIdentifier(BACnetObjectIdentifier objectIdentifier) {
+			this.objectIdentifier = objectIdentifier;
+		}
+
+
+
+		public BACnetPropertyIdentifier getPropertyIdentifier() {
+			return propertyIdentifier;
+		}
+
+
+
+		public void setPropertyIdentifier(BACnetPropertyIdentifier propertyIdentifier) {
+			this.propertyIdentifier = propertyIdentifier;
+		}
+
+
+
+		public BACnetUnsignedInteger getPropertyArrayIndex() {
+			return propertyArrayIndex;
+		}
+
+
+
+		public void setPropertyArrayIndex(BACnetUnsignedInteger propertyArrayIndex) {
+			this.propertyArrayIndex = propertyArrayIndex;
+		}
+
+
+
+		/**
+		 * writes the encoded service into the ByteBuffer
+		 * starting with the Service Choice Element of the APDU
+		 * @param data
+		 */
+		public void encode(final ByteBuf data) {
+			// write service choice
+			data.writeByte(BACnetConfirmedService.Choice.READ_PROPERTY.serviceChoice);
+			objectIdentifier.encode(data,0);
+			propertyIdentifier.encode(data,1);
+		}
+
+
+
+		public byte getServiceChoice() {
+			return BACnetConfirmedService.Choice.READ_PROPERTY.serviceChoice;
+		}
+		
 	}
 	
-	
+	/**
+	 * Response for ReadProperty
+	 * @author Joerg Seitter
+	 *
+	 */
+	public static class ReadPropertyACK {
+		
+		private BACnetObjectIdentifier objectIdentifier; // ctx 0
+		private BACnetPropertyIdentifier propertyIdentifier; // ctx 1
+		private BACnetUnsignedInteger propertyArrayIndex; // ctx 2 optional
+		private BACnetAny propertyValue; // ctx 3
+		
+		public BACnetObjectIdentifier getObjectIdentifier() {
+			return objectIdentifier;
+		}
+
+		public void setObjectIdentifier(BACnetObjectIdentifier objectIdentifier) {
+			this.objectIdentifier = objectIdentifier;
+		}
+
+		public BACnetPropertyIdentifier getPropertyIdentifier() {
+			return propertyIdentifier;
+		}
+
+		public void setPropertyIdentifier(BACnetPropertyIdentifier propertyIdentifier) {
+			this.propertyIdentifier = propertyIdentifier;
+		}
+
+		public BACnetUnsignedInteger getPropertyArrayIndex() {
+			return propertyArrayIndex;
+		}
+
+		public void setPropertyArrayIndex(BACnetUnsignedInteger propertyArrayIndex) {
+			this.propertyArrayIndex = propertyArrayIndex;
+		}
+
+		public BACnetAny getPropertyValue() {
+			return propertyValue;
+		}
+
+		public void setPropertyValue(BACnetAny propertyValue) {
+			this.propertyValue = propertyValue;
+		}
+
+
+		
+		public void encode(final ByteBuf data) {
+			data.writeByte(BACnetConfirmedService.Choice.READ_PROPERTY.serviceChoice);
+			objectIdentifier.encode(data, 0);
+			propertyIdentifier.encode(data, 1);
+			if(propertyArrayIndex!=null) 
+				propertyArrayIndex.encode(data, 2);
+			propertyValue.encode(data, 3);
+			
+		}
+		
+	}
 	
 
 	
