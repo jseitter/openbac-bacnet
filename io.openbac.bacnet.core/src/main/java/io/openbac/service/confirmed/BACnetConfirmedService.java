5937339c74edfd07b22c5715c1e01df284c82f95
J. Seitter <oss@seitter.net>
2023-01-08 14:15:05 +0100

    extended with the implementation class to be sync with unconfirmedSrv

--- io.openbac.bacnet.core/src/main/java/io/openbac/service/confirmed/BACnetConfirmedService.java
+++ io.openbac.bacnet.core/src/main/java/io/openbac/service/confirmed/BACnetConfirmedService.java
@@ -1,18 +1,51 @@
 package io.openbac.service.confirmed;
 
+import io.netty.buffer.ByteBuf;
+import io.openbac.bacnet.exceptions.BACnetParseException;
+
 public abstract class BACnetConfirmedService {
 
+	/**
+	 * Choice to resolve IDs and implementation classes;
+	 * @author joerg
+	 *
+	 */
 	public enum Choice {
 
-		READ_PROPERTY((byte)12);
+		READ_PROPERTY((byte) 12, BACnetReadPropertyService.class);
 
 		// keep in mind that byte is signed
-		byte serviceChoice;
+		public final byte serviceChoice;
+		public final Class implementationClass;
 
-		Choice(byte value) {
-			this.serviceChoice=value;
+		Choice(byte value, Class implementationClass) {
+			this.serviceChoice = value;
+			this.implementationClass = implementationClass;
 		}
-		
+
+		public static Choice forId(byte serviceChoice) {
+
+			Choice[] vals = values();
+
+			for (int i = 0; i < vals.length; i++) {
+				if (serviceChoice == vals[i].serviceChoice)
+					return vals[i];
+
+			}
+			return null;
+		}
 	}
-	
+
+	public static <T extends BACnetConfirmedService> T create(Class<? extends BACnetConfirmedService> clazz,
+			final ByteBuf apdu) throws BACnetParseException {
+		// TODO: does it make sense to parse the service choice here and decide on that
+		// basis ?
+		if (clazz == BACnetReadPropertyService.class) {
+			return (T) new BACnetReadPropertyService(apdu);
+		}
+
+		return null;
+	}
+
+	public abstract byte getServiceChoice();
 }
