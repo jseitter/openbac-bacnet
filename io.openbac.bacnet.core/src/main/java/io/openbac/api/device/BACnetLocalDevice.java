26945b9fea14ed263c484297872d58710a2f575a
J. Seitter <oss@seitter.net>
2023-01-08 14:13:29 +0100

    added comments

--- io.openbac.bacnet.core/src/main/java/io/openbac/api/device/BACnetLocalDevice.java
+++ io.openbac.bacnet.core/src/main/java/io/openbac/api/device/BACnetLocalDevice.java
@@ -1,6 +1,7 @@
 package io.openbac.api.device;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -12,13 +13,14 @@ public class BACnetLocalDevice {
 
 	private static final Logger LOG = LoggerFactory.getLogger(BACnetLocalDevice.class);
 	
+	
 	/**
-	 * The BACnet Device Object containing all BACnet Properties
+	 * The BACnet Device Object for this local device
 	 */
     private final BACnetDeviceObject deviceObject = new BACnetDeviceObject();
 
     /**
-     * 
+     * The list of known remote devices 
      * 
      */
     private List<BACnetRemoteDevice> remoteDeviceList = new ArrayList<>();
