62dedce79b9099a48a18df9f4f80a69f29b502ba
J. Seitter <oss@seitter.net>
2023-01-08 14:15:49 +0100

    comments and initial work for ReadProperty

--- io.openbac.bacnet.core/src/main/java/io/openbac/bacnet/net/apdu/BACnetAPDUHandler.java
+++ io.openbac.bacnet.core/src/main/java/io/openbac/bacnet/net/apdu/BACnetAPDUHandler.java
@@ -8,15 +8,31 @@ import org.slf4j.LoggerFactory;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.DefaultAddressedEnvelope;
+import io.openbac.api.device.BACnetLocalDevice;
 import io.openbac.bacnet.net.apdu.BACnetAPDU.PDUType;
 import io.openbac.bacnet.net.npdu.BACnetNPDU;
+import io.openbac.bacnet.type.primitive.BACnetObjectIdentifier;
+import io.openbac.service.confirmed.BACnetConfirmedService;
+import io.openbac.service.confirmed.BACnetReadPropertyService;
 import io.openbac.service.unconfirmed.BACnetIAmService;
 import io.openbac.service.unconfirmed.BACnetUnconfirmedService;
 import io.openbac.service.unconfirmed.BACnetWhoIsService;
 
+/**
+ * This is the final handler of the network message. Here the APDU is decoded
+ * and the reaction is generated
+ * 
+ * @author Joerg Seitter
+ *
+ */
 public class BACnetAPDUHandler extends ChannelDuplexHandler {
 
 	private static final Logger LOG = LoggerFactory.getLogger(BACnetAPDUHandler.class);
+	private BACnetLocalDevice device;
+
+	public BACnetAPDUHandler(BACnetLocalDevice device) {
+		this.device = device;
+	}
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -25,6 +41,7 @@ public class BACnetAPDUHandler extends ChannelDuplexHandler {
 
 		BACnetAPDU apdu = BACnetAPDU.createAPDU(npdu.getPayload());
 		LOG.debug("APDU received : " + apdu.getPDUType().toString());
+
 		if (apdu.getPDUType() == PDUType.UNCONFIRMED_REQUEST) {
 			BACnetUnconfirmedRequestAPDU uc_apdu = (BACnetUnconfirmedRequestAPDU) apdu;
 			BACnetUnconfirmedService srv = uc_apdu.getService();
@@ -33,32 +50,55 @@ public class BACnetAPDUHandler extends ChannelDuplexHandler {
 			// TODO: solve this with the Choice enum
 			// WHO_IS
 			case 0x08: {
-				LOG.debug("handling WHO IS");
+				LOG.debug("handling WhoIs Service");
+				// TODO do we need to instantiate this at all ?
 				BACnetWhoIsService whois = (BACnetWhoIsService) srv;
 				BACnetIAmService iam = new BACnetIAmService();
 				LOG.debug("sending IAM APDU");
-				ctx.writeAndFlush(
-				new DefaultAddressedEnvelope<BACnetAPDU, SocketAddress>(
-						BACnetUnconfirmedRequestAPDU.createAPDUForService(iam),
-						npdu.getBvll().getSourceAddress())
-				);
+				ctx.writeAndFlush(new DefaultAddressedEnvelope<BACnetAPDU, SocketAddress>(
+						BACnetUnconfirmedRequestAPDU.createAPDUForService(iam), npdu.getBvll().getSourceAddress()));
 
 				break;
 			}
 			}
 		}
 
+		if (apdu.getPDUType() == PDUType.CONFIRMED_REQUEST) {
 
+			BACnetConfirmedRequestAPDU c_apdu = (BACnetConfirmedRequestAPDU) apdu;
+			BACnetConfirmedService srv = c_apdu.getService();
 
-		// find neccesary listeners that shall be called
+			switch (srv.getServiceChoice()) {
 
-		// find out what is the return we shall send
+			case 0x0C: {
+				LOG.debug("handling ReadProperty Service");
+				BACnetReadPropertyService rp_srv = (BACnetReadPropertyService) srv;
+				BACnetAPDU result = handleReadPropertyService(rp_srv);
+				ctx.writeAndFlush(new DefaultAddressedEnvelope<BACnetAPDU, SocketAddress>(result,
+						npdu.getBvll().getSourceAddress()));
 
-		// ctx.writeAndFlush();
-//		if(msg instanceof IAmService) {
-//		LOG.debug("Send Iam Service");
-		// UnconfirmedRequestAPDU apdu = new Un
+			}
 
+			}
+		}
+	}
+
+	private BACnetAPDU handleReadPropertyService(BACnetReadPropertyService srv) {
+
+		BACnetObjectIdentifier id = srv.getObjectIdentifier();
+		switch (id.getObjectType()) {
+
+		case DEVICE: {
+			
+			break;
+		}
+
+		default: {
+			// create error
+		}
+
+		}
+		return null;
 	}
 
 }
