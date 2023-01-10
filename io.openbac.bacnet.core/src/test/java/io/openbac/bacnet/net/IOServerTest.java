e386385a91bc631b374847fd286585f36926431a
J. Seitter <oss@seitter.net>
2023-01-10 22:24:13 +0100

    replaced test with a simple main

--- io.openbac.bacnet.core/src/test/java/io/openbac/bacnet/net/IOServerTest.java
+++ io.openbac.bacnet.core/src/test/java/io/openbac/bacnet/net/IOServerTest.java
@@ -2,30 +2,12 @@ package io.openbac.bacnet.net;
 
 import java.io.IOException;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
 public class IOServerTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		
-	
-		
-	}
+	public static void main(String[] args) throws IOException, InterruptedException {
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Test
-	public void test() throws IOException, InterruptedException {
-		
 		BACnetIOServer srv = new BACnetIOServer("192.168.60.26", BACnetIOServer.BACNET_DEFAULT_PORT0);
-		//srv.run();
-		
-	}
+		srv.run();
 
+	}
 }
- 
